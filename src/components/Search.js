569e44a1a9b06e89dc2fd8e5caca297bd9b7c5f4
natebakescakes <natebakescakes@gmail.com>
2018-06-01 10:54:05 +0800

    Refactor form groups into react-boostrap components

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,37 +1,43 @@
 import React from 'react';
 
+import {
+    FormGroup,
+    FormControl,
+} from 'react-bootstrap';
+
 import ImageGrid from "./ImageGrid";
 
 class Search extends React.Component {
     render() {
         return (
             <div>
-                <form className="form-group">
-                    <input 
-                        className="form-control" 
-                        type="text" 
-                        onChange={this.props.handleTextChange}
-                        value={this.props.query}
-                        placeholder="Start searching for images!"
-                    />
-                    <input 
-                        type="submit" 
-                        style={{ display: "none" }}
-                        onClick={this.props.handleQuerySubmit} 
-                        value="API_GET"
-                    />
-                </form>
+                <FormGroup>
+                    <form>
+                        <FormControl
+                            type="text" 
+                            onChange={this.props.handleTextChange}
+                            value={this.props.query}
+                            placeholder="Start searching for images!"
+                        />
+                        <FormControl
+                            type="submit" 
+                            style={{ display: "none" }}
+                            onClick={this.props.handleQuerySubmit} 
+                            value="API_GET"
+                        />
+                    </form>
+                </FormGroup>
 
                 <ImageGrid handleImageClick={this.props.handleImageClick} imageResults={this.props.imageResults} />
 
                 {this.props.imageResults.length > 0 &&
-                    <form className="form-group">
-                        <input 
+                    <FormGroup>
+                        <FormControl 
                             type="submit"
-                            className="btn btn-primary"
                             onClick={this.props.handleFetchMore} 
-                            value="Fetch More" />
-                    </form>
+                            value="Fetch More" 
+                        />
+                    </FormGroup>
                 }
             </div>
         );
